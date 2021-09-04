281a728d5fbb43c8b1c281e98190fa917da6efc2
jordyf15 <jordyferdian88@gmail.com>
2021-09-04 18:59:06 +0700

    added save and load feature

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
-require_relative 'string'
+require_relative 'string.rb'
+require 'yaml'
 
 class Game
-  attr_reader :correct_letters
   def initialize
     @incorrect_guesses = 10
     @guessed_letters = []
@@ -9,20 +9,21 @@ class Game
     @correct_letters = @word.map{'_'}
   end
 
-  def play?
+  def play
     until @incorrect_guesses == 0
       puts "Letters guessed:  #{@guessed_letters.join(' ')}"
       puts "Incorrect guesses remaining: #{@incorrect_guesses}".yellow
       print_correct_letters
       guess = input_guess
+      return "save" if guess == "save"
       check_guess(guess)
       if @word == @correct_letters
         puts @word.join('').blue
-        return true
+        return "win"
       end
     end
     puts "The answer is: #{@word.join('')}".blue
-    return false
+    return "lose"
   end
    
   def input_guess
@@ -73,12 +74,47 @@ class Game
 end
 
 def main
-  game = Game.new
-  victory = game.play?
-  if victory == true
-    puts "You won the game, congratulations."
+  puts "Welcome to Hangman, Would you like to: "
+  puts "1. Start a new Game."
+  puts "2. Load a Game."
+  input = gets.chomp.to_i
+  until input == 1 || input ==2 
+    puts "Please input between 1 for New Game or 2 for Load Game".red
+    input = gets.chomp.to_i
+  end
+  if input == 1
+    game = Game.new
+    result = game.play
   else
+    puts "Here are the current saved games."
+    files = Dir.glob('saves/*').map {|file_path| file_path[6..-1]}
+    files.each_with_index {|value, index| puts "#{index+1}. #{value}"}
+    print "Please choose the one you want to load (1-#{files.size}): "
+    choice = gets.chomp.to_i
+    until choice >= 1 && choice <= files.size
+      print "File doesn't exist. Please choose an existing file: ".red
+      choice = gets.chomp.to_i
+    end
+    serialized_game_data = File.read("saves/#{files[choice-1]}")
+    game = YAML::load(serialized_game_data)
+    puts "\n\n"
+    puts "#{files[choice-1]} loaded"
+    result = game.play
+  end
+  
+  if result == "win"
+    puts "You won the game, congratulations."
+  elsif result == "lose"
     puts "You lose, good luck next time."
+  else
+    print "Enter a name for your game file: "
+    file_name = gets.chomp
+    until file_name.empty? == false
+      puts "Please enter the name of your game file, it cannot be empty!".red
+      file_name = gets.chomp
+    end
+    serialized_game = game.to_yaml
+    File.write("saves/#{file_name}", serialized_game)
   end
 end
 
